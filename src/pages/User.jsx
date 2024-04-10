efa09b5f6c63d71adcdcdc4f3500208a58b1664d
Nafis Ahamed <nafisahamed14@gmail.com>
2024-04-10 20:03:10 +0600

    user profile

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,15 +1,44 @@
 
+import { useContext } from 'react';
 import { Helmet } from 'react-helmet';
+import { AuthContext } from '../provider/Authprovider';
 
 const User = () => {
+    const {users} = useContext(AuthContext)
+    let name = ""
+    let emails = ""
+    let verified = ""
+    let images = ""
+    let id = ""
+    if(users){
+        const {displayName,email,emailVerified,photoURL,uid} = users
+        name = displayName
+        emails = email 
+        verified = emailVerified
+        images = photoURL 
+        id = uid
+        
+    }
     return (
         <div>
             <Helmet>
                <title>User page</title>
             </Helmet>
-           <h2 className="text-2xl">users</h2> 
+           <div className='container mx-auto'>
+             
+             <img src={images} className='md:ml-[250px] lg:ml-[700px] ml-[50px] mt-10 rounded-full' alt="" />
+             <h2 className="lg:text-3xl md:text-2xl text-xl font-bold md:ml-[200px] lg:ml-[620px] ml-[20px] mt-5 text-blue-400">
+               <span>Name:</span>  {name}</h2>
+             <h2 className="lg:text-3xl md:text-2xl text-xl font-bold md:ml-[200px] lg:ml-[620px] ml-[20px] mt-5 text-red-600">
+               <span>Email:</span> {emails}</h2>
+               <h2 className="lg:text-3xl md:text-2xl text-xl font-bold md:ml-[200px] lg:ml-[620px] ml-[20px] mt-5 text-purple-400">
+               <span>Verified:</span>  {verified ? <span>true</span> : <span>false</span> }</h2>
+               <h2 className="lg:text-3xl md:text-2xl text-xl font-bold md:ml-[200px] lg:ml-[620px] ml-5 mt-5 ">
+               <span>Id:</span>  {id}</h2>
+
+            </div> 
         </div>
     );
 };
 
-export default User;
+export default User; 
